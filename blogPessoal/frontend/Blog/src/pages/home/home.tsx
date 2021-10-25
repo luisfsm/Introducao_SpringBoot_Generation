a3bd026d353a3555893518ce007b7da749f5cabb
luis felipe <luis.s.mendes20@gmail.com>
2021-10-25 16:33:42 -0300

    add front end

---
+++ blogPessoal/frontend/Blog/src/pages/home/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './home.css'
+
+function home() {
+    return (
+        <div>
+            <h1 className='titulo'>Home</h1>
+            <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem inicial"  className='img'/>
+
+        </div>
+    );
+}
+
+export default home;
